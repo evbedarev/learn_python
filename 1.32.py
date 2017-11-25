9ed38f34585c9e9b0083d4f6a6a6be136571fdd8
madjo <madjo85@yandex.ru>
2017-11-25 22:10:25 +0300

    ready

--- 1.32.py
+++ 1.32.py
@@ -1,20 +1,24 @@
 import re
-def book(http, region, out):
+def book(region, out):
     lnk = 'http://amazon.XX/dp/0132678209'
     if region in ['de','fr','jp','cn','co.uk']:
         lnk = re.sub('XX', region, lnk)
     else:
         lnk = re.sub('XX', 'com', lnk)
-    print(lnk)
+    text = ttt(lnk)
+    if out == 'text':
+        print(text)
+    else:
+        f = open('c:\TEMP\out.html')
+        f.writelines('<p>' + text + '</p>')
 
-def ttt():
-    from lxml import html, etree
+def ttt(lnk):
+    from lxml import html
     import urllib.request as request
     from urllib import request
     from urllib.parse import urlparse
-    response = request.urlopen('https://www.amazon.com/dp/0132678209', timeout=40)
+    response = request.urlopen(lnk, timeout=40)
     page = html.parse(response)
     e = page.getroot().find_class('a-size-medium a-color-price header-price').pop()
     print(e.text.strip())
-
-ttt()
+    return e.text.strip()
