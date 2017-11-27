5b7b754be1d96f7a1905f8caaaccc2986023dca7
mj <madjo85@yandex.ru>
2017-11-27 14:17:17 +0300

    ready gui changename rm conf

--- 1.32.py
+++ 1.32.py
@@ -5,20 +5,28 @@ def book(region, out):
         lnk = re.sub('XX', region, lnk)
     else:
         lnk = re.sub('XX', 'com', lnk)
-    text = ttt(lnk)
+    text = ttt(lnk, 0)
     if out == 'text':
         print(text)
     else:
         f = open('c:\TEMP\out.html')
         f.writelines('<p>' + text + '</p>')
 
-def ttt(lnk):
+def ttt(lnk,i):
     from lxml import html
-    import urllib.request as request
     from urllib import request
-    from urllib.parse import urlparse
-    response = request.urlopen(lnk, timeout=40)
+    import time
+    if i ==0: i = 1
+    if i == 5: return 'Can not connect to link ' + lnk
+    try:
+        response = request.urlopen(lnk, timeout=40)
+    except Exception:
+        i = i + 1
+        ttt(lnk, i)
+
     page = html.parse(response)
     e = page.getroot().find_class('a-size-medium a-color-price header-price').pop()
     print(e.text.strip())
     return e.text.strip()
+
+book('com', 'text')
