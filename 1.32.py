ecc107fc50d4e9b9e87120d233ae8d100a9c7cb1
mj <madjo85@yandex.ru>
2017-11-27 14:46:50 +0300

    ready gui changename rm conf

--- 1.32.py
+++ 1.32.py
@@ -1,7 +1,10 @@
 import re
+
+
 def book(region, out):
+
     lnk = 'http://amazon.XX/dp/0132678209'
-    if region in ['de','fr','jp','cn','co.uk']:
+    if region in ['de', 'fr', 'jp', 'cn', 'co.uk']:
         lnk = re.sub('XX', region, lnk)
     else:
         lnk = re.sub('XX', 'com', lnk)
@@ -12,21 +15,28 @@ def book(region, out):
         f = open('c:\TEMP\out.html')
         f.writelines('<p>' + text + '</p>')
 
-def ttt(lnk,i):
+
+def ttt(lnk, i):
+
     from lxml import html
     from urllib import request
     import time
-    if i ==0: i = 1
-    if i == 5: return 'Can not connect to link ' + lnk
-    try:
-        response = request.urlopen(lnk, timeout=40)
-    except Exception:
-        i = i + 1
-        ttt(lnk, i)
+
+    # if i == 0: i = 1
+    # if i == 5: return 'Can not connect to link ' + lnk
+    response = ''
+    while response == '':
+        try:
+            response = request.urlopen(lnk, timeout=5)
+            print('go to link ' + lnk)
+
+        except Exception:
+            print('Service unavailble')
+            time.sleep(2)
+
 
     page = html.parse(response)
     e = page.getroot().find_class('a-size-medium a-color-price header-price').pop()
-    print(e.text.strip())
     return e.text.strip()
 
-book('com', 'text')
+book('com','text')
