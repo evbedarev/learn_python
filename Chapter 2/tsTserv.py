d94585066fcc982e8a406a7c04105b7150eeebfc
mj <madjo85@yandex.ru>
2017-11-30 11:05:31 +0300

    ready gui changename rm conf Start Chapter2

--- Chapter 2/tsTserv.py
+++ Chapter 2/tsTserv.py
@@ -1,24 +1,29 @@
 from socket import *
 from time import ctime, sleep
 HOST = '172.29.98.0'
-PORT = 21566
+PORT = 21567
 BUFSIZE = 1024
-ADDR = (HOST,PORT)
+ADDR = (HOST, PORT)
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
-while True:
-    print('waiting for connection...')
-    tcpCliSock, addr = tcpSerSock.accept()
-    print('...connected from:', addr)
+try:
     while True:
-        data = tcpCliSock.recv(BUFSIZE)
-        if not data:
-            break
-        print(data)
-        tcpCliSock.send(bytes(str(ctime()) + ' ' + data.decode('utf-8'), 'utf-8'))
+        print('waiting for connection...')
+        tcpCliSock, addr = tcpSerSock.accept()
+        print('...connected from:', addr)
+        while True:
+            data = tcpCliSock.recv(BUFSIZE)
+            if not data:
+                break
+            print(data)
+            tcpCliSock.send(bytes('[' + str(ctime()) + ']' + ' ' + data.decode('utf-8'), 'utf-8'))
 
-    tcpCliSock.close()
+        tcpCliSock.close()
 
-tcpSerSock.close()
+except KeyboardInterrupt:
+    tcpSerSock.close()
+
+finally:
+    tcpSerSock.close()
